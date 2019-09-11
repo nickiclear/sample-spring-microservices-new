7cc4ae6a4126e8d0ff2bf08af1df2de1b203e952
nickiclear <47432837+nickiclear@users.noreply.github.com>
2019-09-11 16:06:30 -0400

    Update EmployeeApplication.java

--- employee-service/src/main/java/pl/piomin/services/employee/EmployeeApplication.java
+++ employee-service/src/main/java/pl/piomin/services/employee/EmployeeApplication.java
@@ -30,7 +30,7 @@ public class EmployeeApplication {
 					.apis(RequestHandlerSelectors.basePackage("pl.piomin.services.employee.controller"))
 					.paths(PathSelectors.any())
 				.build()
-				.apiInfo(new ApiInfoBuilder().version("1.0").title("Employee API").description("Documentation Employee API v1.0").build());
+				.apiInfo(new ApiInfoBuilder().version("1.0").title("Employee API").description("Documentation Employee API v2.0").build());
 	}
 	
 	@Bean
